48bdf06683097ca4a4b14cd27ec3753a208fec9d
William Nogueira <wdsn.dev@gmail.com>
2023-04-06 19:41:53 -0300

    primeiro commit

---
+++ src/components/App.jsx
@@ -0,0 +1,35 @@
+import Contact from "./Contact";
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="contacts">
+      <Contact
+        img="./mr-whiskerson.png"
+        name="Mr. Whiskerson"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+      />
+      <Contact
+        img="./fluffykins.png"
+        name="Fluffykins"
+        phone="(212) 555-2345"
+        email="fluff@me.com"
+      />
+      <Contact
+        img="./felix.png"
+        name="Felix"
+        phone="(212) 555-4567"
+        email="thecat@hotmail.com"
+      />
+      <Contact
+        img="./pumpkin.png"
+        name="Pumpkin"
+        phone="(0800) CAT KING"
+        email="pumpkin@gmail.com"
+      />
+    </div>
+  );
+}
+
+export default App;
